73c7c905e170dcde6275361a69486c8d41dfb11b
Kiranaaa <putrikirana203@gmail.com>
2026-07-11 15:18:43 +0800

    memperbaiki detail pada post agar lebih lengkap dan rapi

--- lib/features/home/widgets/home_event_section.dart
+++ lib/features/home/widgets/home_event_section.dart
@@ -33,16 +33,13 @@ class _HomeEventSectionState extends State<HomeEventSection> {
     ),
   ];
 
-
   @override
   void initState() {
     super.initState();
-
     _pageController = PageController(
       viewportFraction: 0.78,
     );
   }
-
 
   @override
   void dispose() {
@@ -50,17 +47,13 @@ class _HomeEventSectionState extends State<HomeEventSection> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-
       children: [
-
         SectionHeader(
           title: "Upcoming Events",
-
           onViewAll: () {
             Navigator.pushNamed(
               context,
@@ -68,331 +61,139 @@ class _HomeEventSectionState extends State<HomeEventSection> {
             );
           },
         ),
-
-
         const SizedBox(height: 16),
-
-
-
         SizedBox(
           height: 230,
-
           child: PageView.builder(
-
             controller: _pageController,
-
             padEnds: false,
-
             itemCount: events.length,
-
-
             onPageChanged: (index) {
-
               setState(() {
                 currentIndex = index;
               });
-
             },
-
-
             itemBuilder: (_, index) {
-
               return Padding(
-
-                padding: const EdgeInsets.only(
-                  right: 16,
-                ),
-
-
+                padding: const EdgeInsets.only(right: 16),
                 child: Align(
-
                   alignment: Alignment.centerLeft,
-
-
                   child: _EventCard(
                     event: events[index],
                   ),
-
                 ),
-
               );
-
             },
-
           ),
         ),
-
-
-
         const SizedBox(height: 16),
-
-
-
         Row(
-
           mainAxisAlignment: MainAxisAlignment.center,
-
-
           children: List.generate(
-
             events.length,
-
-
             (index) {
-
               return AnimatedContainer(
-
-                duration:
-                    const Duration(milliseconds: 250),
-
-
-                margin:
-                    const EdgeInsets.symmetric(
-                  horizontal: 4,
-                ),
-
-
-                width: currentIndex == index
-                    ? 22
-                    : 8,
-
-
+                duration: const Duration(milliseconds: 250),
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                width: currentIndex == index ? 22 : 8,
                 height: 8,
-
-
                 decoration: BoxDecoration(
-
                   color: currentIndex == index
                       ? Colors.green
                       : Colors.grey.shade300,
-
-
-                  borderRadius:
-                      BorderRadius.circular(20),
-
+                  borderRadius: BorderRadius.circular(20),
                 ),
-
               );
-
             },
-
           ),
-
         ),
-
       ],
     );
   }
 }
 
-
-
 class _EventCard extends StatelessWidget {
-
   final _HomeEvent event;
-
 
   const _EventCard({
     required this.event,
   });
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return Container(
-
       width: 250,
-
-
       decoration: BoxDecoration(
-
         color: Colors.white,
-
-
-        borderRadius:
-            BorderRadius.circular(22),
-
-
+        borderRadius: BorderRadius.circular(22),
         boxShadow: [
-
           BoxShadow(
-
-            color:
-                Colors.black.withOpacity(.05),
-
-
+            color: Colors.black.withOpacity(.05),
             blurRadius: 10,
-
-
-            offset:
-                const Offset(0, 4),
-
+            offset: const Offset(0, 4),
           ),
-
         ],
-
       ),
-
-
-
       child: Column(
-
-        crossAxisAlignment:
-            CrossAxisAlignment.start,
-
-
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-
           ClipRRect(
-
-            borderRadius:
-                const BorderRadius.vertical(
-
+            borderRadius: const BorderRadius.vertical(
               top: Radius.circular(22),
-
             ),
-
-
             child: Image.asset(
-
               event.image,
-
-
               height: 145,
-
-
               width: 250,
-
-
               fit: BoxFit.cover,
-
             ),
-
           ),
-
-
-
-
           Padding(
-
-            padding:
-                const EdgeInsets.all(16),
-
-
-
+            padding: const EdgeInsets.all(16),
             child: Column(
-
-              crossAxisAlignment:
-                  CrossAxisAlignment.start,
-
-
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
-
-
                 Text(
-
                   event.title,
-
-
-                  style:
-                      const TextStyle(
-
+                  style: const TextStyle(
                     fontSize: 17,
-
-                    fontWeight:
-                        FontWeight.bold,
-
+                    fontWeight: FontWeight.bold,
                   ),
-
                 ),
-
-
-
-
                 const SizedBox(height: 8),
-
-
-
-
                 Row(
-
                   children: [
-
-
                     const Icon(
-
                       Icons.calendar_month,
-
                       size: 16,
-
                       color: Colors.grey,
-
                     ),
-
-
-
                     const SizedBox(width: 5),
-
-
-
-
                     Text(
-
                       event.date,
-
-
-                      style:
-                          const TextStyle(
-
+                      style: const TextStyle(
                         color: Colors.grey,
-
                       ),
-
                     ),
-
                   ],
-
                 ),
-
               ],
-
             ),
-
           ),
-
         ],
-
       ),
-
     );
-
   }
 }
 
-
-
 class _HomeEvent {
-
   final String title;
-
   final String date;
-
   final String image;
 
-
-
   const _HomeEvent({
-
     required this.title,
-
     required this.date,
-
     required this.image,
-
   });
-
 }
